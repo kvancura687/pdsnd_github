d2ba07a226a501906301c5a4654e4b16b253b59b
Katie_Vancura <kvancura.ctr@kr.af.mil>
2024-10-04 11:38:43 -0400

    Add Atlanta information

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -4,7 +4,8 @@ import numpy as np
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }
+              'washington': 'washington.csv',
+              'atlanta': 'atlanta.csv' }
 
 def get_filters():
     """
@@ -16,7 +17,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington, atlanta). HINT: Use a while loop to handle invalid inputs
 
 
     # get user input for month (all, january, february, ... , june)
